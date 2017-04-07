06378d4b8331286d8cd4d7d73cb4415b674984ff
hanksterhan <henry.s.han@hotmail.com>
2017-04-06 20:46:28 -0400

    tested the scaled up model on a single player.
    Wrote the framework for scaling up to the entire NBA. Going to insert
    player name and player ID to each line in the Database. Also need to
    figure out how to read from two files simultaneously, however.

--- learningPythonScripts.py
+++ learningPythonScripts.py
@@ -4,18 +4,19 @@ from urllib2 import urlopen
 
 BASE_URL = "http://www.basketball-reference.com/players/{player_id}/gamelog/2017/"
 
-conn = sqlite3.connect('CarmeloAnthony.db')
+# conn = sqlite3.connect('CarmeloAnthony.db')
+conn = sqlite3.connect('LaMarcusAldridge.db')
 c = conn.cursor()
 
 active_players = []
 active_players_ids = []
 
 ##Creates the table will all the columns we need
-##Does not create if the table already exists
+##Does not create if the table already exist
 ##
 def create_table():
 	#Use all caps for pure sql and lower case for things that aren't
-	c.execute('CREATE TABLE IF NOT EXISTS stuffToPlot(game_season TEXT, data_game TEXT, age TEXT, team_id TEXT, game_location TEXT, opp_id TEXT, game_result TEXT, gs TEXT, mp TEXT, fg TEXT, fga TEXT, fg_pct TEXT, fg3 TEXT, fg3a TEXT, fg3_pct TEXT, ft TEXT, fta TEXT, ft_pct TEXT, orb TEXT, drb TEXT, trb TEXT, ast TEXT, stl TEXT, blk TEXT, tov TEXT, pf TEXT, pts TEXT, game_score TEXT, plus_minus TEXT)')
+	c.execute('CREATE TABLE IF NOT EXISTS stuffToPlot(player_id TEXT, game_season TEXT, data_game TEXT, age TEXT, team_id TEXT, game_location TEXT, opp_id TEXT, game_result TEXT, gs TEXT, mp TEXT, fg TEXT, fga TEXT, fg_pct TEXT, fg3 TEXT, fg3a TEXT, fg3_pct TEXT, ft TEXT, fta TEXT, ft_pct TEXT, orb TEXT, drb TEXT, trb TEXT, ast TEXT, stl TEXT, blk TEXT, tov TEXT, pf TEXT, pts TEXT, game_score TEXT, plus_minus TEXT)')
 
 ## I don't know if I still need this. 
 def dynamic_data_entry():
@@ -40,7 +41,7 @@ def get_active_players(letter):
 def extractPlayerIds():
 	for i in range(len(active_players_ids)):
 		temp = active_players_ids[i]
-		active_players_ids[i] = temp[8:(len(temp)-5)]
+		active_players_ids[i] = temp[9:(len(temp)-5)]
 
 ## Takes all the IDs and writes it into playerIDs.txt
 def writePlayerIdstoFile():
@@ -49,7 +50,12 @@ def writePlayerIdstoFile():
 		my_file.write(str(playerID) + "\n")
 	my_file.close()
 
-
+## Takes all the names and writes it into playerNames.txt
+def writePlayerNamestoFile():
+	my_file = open("playerNames.txt", "r+")
+	for playerName in active_players:
+		my_file.write(str(playerName) + "\n")
+	my_file.close()
 
 ##helper function to populate an array with an alphabet
 ##so that we can easily find all the active players
@@ -66,26 +72,24 @@ def populateArrayWithAlphabet():
 ##Need to add columns like player name and payer ID
 def get_game_data(playerID):
 
-	playerStats = []
 	gameIDs = []
 
 	gameIDs = get_game_IDs(playerID)
 
 	soup = make_soup(BASE_URL.format(player_id = playerID))
-	## Figure out this nested for loop
 	##First for loop goes vertically and captures each game played in a season
 	##Second goes horizontally and captures each stat accumulated in a game
 	for gameID in gameIDs:
-		tbody = soup.find(id=gameID)
-		#Error on this line
-		# tbody2 = tbody.findALL("td")
+		playerStats = []
+		playerStats.append(gameID)
+		tbody = soup.find(id=gameID.encode('utf-8'))
 		for td in tbody.findAll("td"):
-			#print td.get("data-stat") + ":" + td.text
 			playerStats.append(td.text.encode('utf-8'))
-		c.executemany("INSERT INTO stuffToPlot (game_season, data_game, age, team_id, game_location, opp_id, game_result, gs, mp, fg, fga, fg_pct, fg3, fg3a, fg3_pct, ft, fta, ft_pct, orb, drb, trb , ast, stl, blk, tov, pf, pts, game_score, plus_minus) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", (playerStats,))
-		#variables: game_season, data_game, age, team_id, game_location, opp_id, game_result, gs, mp, fg, fga, fg3, fg3a, fg3_pct, ft, fta, ft_pct, orb, drb, trb , ast, stl, blk, tov, pf, pts, game_score, plus_minus
+		c.executemany("INSERT INTO stuffToPlot (player_id, game_season, data_game, age, team_id, game_location, opp_id, game_result, gs, mp, fg, fga, fg_pct, fg3, fg3a, fg3_pct, ft, fta, ft_pct, orb, drb, trb , ast, stl, blk, tov, pf, pts, game_score, plus_minus) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", (playerStats,))
+	# 	#variables: game_season, data_game, age, team_id, game_location, opp_id, game_result, gs, mp, fg, fga, fg3, fg3a, fg3_pct, ft, fta, ft_pct, orb, drb, trb , ast, stl, blk, tov, pf, pts, game_score, plus_minus
 		conn.commit()
-	
+
+
 ##helper function
 def make_soup(url):
 	html = urlopen(url).read()
@@ -111,10 +115,12 @@ def main():
 	# for id in ids:
 	# 	get_game_data(id)
 	# 	dynamic_data_entry()
-	# get_active_players()
+	#   get_active_players()
+	file = open("playerIDs.txt","r")
+	for line in file:
+		get_game_data(line)
 
-	get_game_data("a/aldrila01")
-
+	# file.close()
 	########################################################
 	####        Extracted all the player IDs already    ####
 	########################################################
@@ -124,6 +130,7 @@ def main():
 	# 	get_active_players(alphabet[i])
 	# extractPlayerIds()
 	# writePlayerIdstoFile()
+	# writePlayerNamestoFile()
 
 	# dynamic_data_entry()
 	c.close()
